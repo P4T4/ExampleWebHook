6c33e49c7af4bb91530882c8c22b9e97f9d2b775
P4t4 <andresfelipepatarroyo@gmail.com>
2024-03-05 11:35:54 -0500

    Fix: add line

--- routes/webhook.js
+++ routes/webhook.js
@@ -1,11 +1,10 @@
 var express = require("express");
 var router = express.Router();
 
-/* GET users listing. */
+/* POST webhook listing. */
 router.post("/", function (req, res, next) {
   const { data } = req;
   console.log(data);
-  console.log(res);
   res.send("respond with a resource");
 });
 
